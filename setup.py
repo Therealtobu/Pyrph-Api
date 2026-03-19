b8d947e0283323105eee0900597d5a69f376d510
TobuTheXd <phamthien332013@gmail.com>
2026-03-19 13:49:00 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Pyrph",
-    version="1.0.1",
+    version="1.0.2",
     description="Python Obfuscation Engine — VM + Native + MBA",
     author="Therealtobu",
     packages=find_packages(),
